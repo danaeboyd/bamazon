2d3c027a49a503bad1bdaa57d761edb5e10390ac
Danae Boyd <danae.boyd@gmail.com>
2017-07-10 23:21:57 -0700

    updated js code - listProducts

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,9 +6,20 @@ var connection = mysql.createConnection ({
 	port: 3306,
 	user: "root",
 	password: "Zzxc12asd!!",
-	database: "bamazon"
+	database: "bamazon_db"
 	});
 
 connection.connect(function(err) {
 	if (err) throw err;
+	listProducts();
 	});
+
+
+var listProducts = function() {
+	var query = "SELECT * FROM products"
+	connection.query(query, function(err,res) {
+		for(var i = 0; i < res.length; i++) {
+			console.log("Item ID: " + res[i].item_id + " || Product: " + res[i].product_name + " || Department: " + res[i].department_name)
+		}
+	});
+}
