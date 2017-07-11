de96bcefe48e67ca01f71678561551697813982f
Danae Boyd <danae.boyd@gmail.com>
2017-07-10 23:44:35 -0700

    added shoppingCart function to js file and screenshot images of node.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,5 +21,30 @@ var listProducts = function() {
 		for(var i = 0; i < res.length; i++) {
 			console.log("Item ID: " + res[i].item_id + " || Product: " + res[i].product_name + " || Department: " + res[i].department_name)
 		}
+		shoppingCart();
 	});
 }
+
+var shoppingCart = function() {
+	inquirer.prompt([{
+		name: "ProductID",
+		type: "input",
+		message: "What is the ID of the product you would like to buy?",
+	}, {
+		name: "Quantity",
+		type: "input",
+		message: "How many would you like to buy?",
+	}]).then(function(answer) {
+		var query = "SELECT * FROM products WHERE item_id" + answer.Quantity;
+		connection.query(query, function(err, res) {
+			if(answer.Quantity <= res) {
+				for (var i = 0; i < res.length; i++) {
+					console.log("We currently have " + res[i].stock_quantity + " " + res[i].product_name + ".");
+					console.log("Thank you for your purchase!  Your order of "+ res[i].stock_quantity + " " + res[i].product_name + " is now being fulfilled.");
+				}
+			} else {
+				console.log("We are currently out of stock of this product.");
+			}
+		})
+	})
+};
